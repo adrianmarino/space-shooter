1b0783f450ac2ec86ce66046fb438034d9a87d63
adrianmarino <adrianmarino@gmail.com>
2017-03-24 13:42:03 -0300

    destroy explosions

---
+++ Assets/Scripts/element/event/DestroyByTime.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+	void Start ()
+	{
+		Destroy (gameObject, LifeTime);	
+	}
+
+	//-----------------------------------------------------------------------------
+	// Properties
+	//-----------------------------------------------------------------------------
+
+	public float LifeTime {
+		get { return lifeTime; }
+		set { lifeTime = value; }
+	}
+
+	//-----------------------------------------------------------------------------
+	// Attributes
+	//-----------------------------------------------------------------------------
+
+	[SerializeField]
+	private float lifeTime;
+
+	//--------------------------------------------------------------
+	// Constructors
+	//--------------------------------------------------------------
+
+	public DestroyByTime ()
+	{
+		lifeTime = 3;
+	}
+}
+
